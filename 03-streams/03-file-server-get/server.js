e5d18a0e667c9ee3a807a9dcd79d6468e50a7d60
s.laptev <s.laptev@agatgroup.com>
2022-01-24 13:45:59 +0300

    Solution module 3 - 03-file-server-get refactor

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -10,47 +10,28 @@ server.on('request', (req, res) => {
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
-  const file = [];
-
-  function errorHandler(cb) {
-    try {
-      cb();
-    } catch (e) {
-      console.error('Error:', e.message);
-      res.statusCode = 500;
-      res.end('Server error');
-    }
-  }
-
   switch (req.method) {
     case 'GET':
-      fs.createReadStream(filepath)
-          .on('data', (chunk) => {
-            errorHandler(() => {
-              // console.log(chunk);
-              file.push(chunk);
-            });
-          })
-          .on('error', (err) => {
-            errorHandler(() => {
-            // console.log('error', err);
-              if (err.code === 'ENOENT') {
-                if (pathname.includes('/')) {
-                  res.statusCode = 400;
-                  res.end('Bad request');
-                } else {
-                  res.statusCode = 404;
-                  res.end('File not found');
-                }
-              }
-            });
-          })
-          .on('end', ()=> {
-            res.end(Buffer.concat(file));
-          })
-          .on('close', ()=> {
-            // console.log('close');
-          });
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+      stream.on('error', (err) => {
+        if (err.code === 'ENOENT') {
+          if (pathname.includes('/')) {
+            res.statusCode = 400;
+            res.end('Bad request');
+          } else {
+            res.statusCode = 404;
+            res.end('File not found');
+          }
+        } else {
+          console.error('Error:', e.message);
+          res.statusCode = 500;
+          res.end('Server error');
+        }
+      }).on('aborted', () => {
+        stream.destroy();
+      });
+
       break;
 
     default:
