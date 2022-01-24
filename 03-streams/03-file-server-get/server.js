05708c87dcfc6f5db1e0f0e8a127b0813c5fb700
s.laptev <s.laptev@agatgroup.com>
2022-01-24 12:10:02 +0300

    Solution module 3 - 03-file-server-get

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -9,10 +10,47 @@ server.on('request', (req, res) => {
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
+  const file = [];
+
+  function errorHandler(cb) {
+    try {
+      cb();
+    } catch (e) {
+      console.error('Error:', e.message);
+      res.statusCode = 500;
+      res.end('Server error');
+    }
+  }
 
   switch (req.method) {
     case 'GET':
-
+      fs.createReadStream(filepath)
+          .on('data', (chunk) => {
+            errorHandler(() => {
+              // console.log(chunk);
+              file.push(chunk);
+            });
+          })
+          .on('error', (err) => {
+            errorHandler(() => {
+            // console.log('error', err);
+              if (err.code === 'ENOENT') {
+                if (pathname.includes('/')) {
+                  res.statusCode = 400;
+                  res.end('Bad request');
+                } else {
+                  res.statusCode = 404;
+                  res.end('File not found');
+                }
+              }
+            });
+          })
+          .on('end', ()=> {
+            res.end(Buffer.concat(file));
+          })
+          .on('close', ()=> {
+            // console.log('close');
+          });
       break;
 
     default:
