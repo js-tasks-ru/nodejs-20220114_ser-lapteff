671519857ada6e1a3989c616219c88a6456a03f1
serlapteff <ser.lapteff@yandex.ru>
2022-02-06 16:31:21 +0300

    solution module 5 - 01-chat-app

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,38 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  await new Promise((resolve, reject) => {
+    subscribers.push({
+      resolve,
+      reject,
+    });
+  })
+      .then((message) => {
+        ctx.status = 200;
+        ctx.response.body = message;
+
+        next();
+      })
+      .catch((error)=> {
+        console.log(error);
+      });
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  ctx.response.status = 200;
+
+  if (!message) return next();
+
+  subscribers.forEach((subscriber) => {
+    subscriber.resolve(message);
+  });
+
+  subscribers = [];
+  next();
 });
 
 app.use(router.routes());
