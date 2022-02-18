4480de6b80330d68e14462c3e37b762549b29ee0
s.laptev <s.laptev@agatgroup.com>
2022-02-18 12:39:50 +0300

    Solution module 8 - 02-order

--- 08-email/02-order/controllers/orders.js
+++ 08-email/02-order/controllers/orders.js
@@ -1,8 +1,39 @@
 const Order = require('../models/Order');
+const Product = require('../models/Product');
 const sendMail = require('../libs/sendMail');
+const mapOrder = require('../mappers/order');
+const mapOrderConfirmation = require('../mappers/orderConfirmation');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  const {product: productId, phone, address} = ctx.request.body;
+  const product = await Product.findOne({id: productId});
+
+  const order = await Order.create({
+    user: ctx.user.id,
+    product,
+    phone,
+    address,
+  });
+
+  const orderProduct = mapOrderConfirmation(order, product);
+
+  if (order) {
+    await sendMail({
+      template: 'order-confirmation',
+      locals: orderProduct,
+      to: ctx.user.email,
+      subject: 'Подтверждение заказа',
+    });
+
+    ctx.status = 200;
+    ctx.body = {order: order.id};
+  }
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  const rawOrders = await Order.find({user: ctx.user.id}).populate('product');
+
+  const orders = rawOrders.map((order) => mapOrder(order));
+
+  ctx.body = {orders};
 };
--- 08-email/02-order/libs/sendMail.js
+++ 08-email/02-order/libs/sendMail.js
@@ -11,9 +11,9 @@ const StubTransport = require('nodemailer-stub-transport');
 const transportEngine = process.env.NODE_ENV === 'test' ?
   new StubTransport() :
   new SMTPTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    // secure: true,
     auth: {
       user: config.mailer.user,
       pass: config.mailer.password,
